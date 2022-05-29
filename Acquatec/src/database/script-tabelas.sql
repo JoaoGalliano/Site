63f1db9f197191064fdaac1205d428f40dcde57d
JoaoGalliano <joao.goliveira@sptech.school>
2022-05-28 23:22:52 -0300

    Tá pegando forma

--- Acquatec/src/database/script-tabelas.sql
+++ Acquatec/src/database/script-tabelas.sql
@@ -5,7 +5,7 @@ use site;
 -- TABELA PERFIL (ADMIN E USUARIO)
 create table perfil(
 idPerfil int primary key auto_increment,
-descPerfil varchar(45)
+descPerfil varchar(45) 
 );
 
 -- INSERIR PERFIL
@@ -16,7 +16,7 @@ insert into perfil values
 ---------------------------------------------------------------------------------------------------------------------------------------
 -- TABELA USUÁRIO (USUÁRIO FINAL)
 create table usuario(
-id int primary key auto_increment,
+idUsuario int primary key auto_increment,
 nome varchar (45),
 email varchar(45),
 senha varchar(30),
@@ -30,34 +30,10 @@ insert into usuario values
 
 ---------------------------------------------------------------------------------------------------------------------------------------
 -- TABELA POST (POSTS, SUAS CURTIDAS, COMENTÁRIOS E DATA)
-create table post(
+create table aviso(
 id int primary key auto_increment,
-dataPost datetime,
 descPost text,
-statusPost boolean default 0,
+statusPost char default 0,
 fkUsuario int,
 foreign key (fkUsuario) references usuario(idUsuario)
 );
-
--- INSERIR POST
-insert into post values 
-(null, 'Aqui você tem um espaço para compartilhar com o mundo o que tem de melhor!', now(), null, 1);
------------------------------------------------------------------------------------------------------------------------------
-
--- TABELA INTERAÇÃO (STATUS DO POST, DATA, QUEM REAGIU/COMENTOU EM QUAL POST)
-
-create table interacao(
-dataInt int auto_increment,
-fkUsuario int,
-foreign key (fkUsuario) references usuario (idUsuario),
-fkPost int,
-foreign key (fkPost) references post (idPost),
-tipo varchar(20), 
-check(tipo = 'Comentário' or tipo = 'Curtida' or tipo = 'Descurtida'),
-descInt text,
-statusInt boolean default 0,
-primary key (dataInt, fkUsuario, fkPost)
-);
--- INSERIR INTERAÇÃO 
-----------------------------------------------------------------------------------------
- select*from usuario join post on idUsuario = fkUsuario;
